fce1647b0db2f7f69990f754d8107344ab38d090
Your Name <you@example.com>
2024-10-30 19:29:22 +0100

    additional function, especially position player

--- poker_game.py
+++ poker_game.py
@@ -1,5 +1,4 @@
-# import random
-
+import random
 
 deck_of_cards_notfull = ["As", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
 colors = ["heart", "spade", "club", "diamond"]
@@ -7,7 +6,9 @@ deck_of_cards = []
 for color in colors:
     for card in deck_of_cards_notfull:
         deck_of_cards.append(card + " of " + color)
-print(deck_of_cards)
+dico_positions = {1: "LowJack", 2: "Hijack", 3: "Cut-Off", 4: "Button", 5: "Small Blind", 6: "Big Blind"}
+
+
 class Player:
     def __init__(self, name):
         self.name = name
@@ -21,25 +22,51 @@ def game_type():
 def add_player():
     list_players = []
     player = ""
-    while player != "ok":
+    while (player != "ok") and (len(list_players) <= 6) and (len(list_players) >= 2):
         player = input("enter player\'s name to add a new player or \'ok\' to continue")
         if player != "ok":
             list_players.append(player)
     return list_players
 
+def initialize_players_position(list_players):
+    players_position = {}
+    count = 1
+    for player in list_players:
+        players_position[count] = player
+        count += 1
+    return players_position
+
+def initialize_players_position_2(list_players):
+    players_position = {player:pos for player, pos in zip(list_players, range(len(list_players)))}
+    return players_position
+
+def change_players_position(suivi_pos):
+    for key in previous_pos.keys():
+        if suivi_pos[key] != len(suivi_pos):
+            suivi_pos[key] += 1
+        elif suivi_pos[key] == len(suivi_pos):
+            suivi_pos[key] = 1
+    return suivi_pos
+
+
+
 def lauch_game():
     game_type = game_type()
     list_players = add_player()
+    LJ_position = random.choice(list_players)
+    players
     # while party continue:
         # new_turn()
 
 def new_turn(list_players, game_type):
     distribution = {}
     for player in list_players:
-        distribution[player] = random.choice(deck_of_cards, k=2)
+        distribution[player] = random.choices(deck_of_cards, k=2)
+    
+    
 
 
-# print(random.choice(1, 2, 3))
+print(len({"a":2, 2:8, "err": "efff"}))
 
 
 
