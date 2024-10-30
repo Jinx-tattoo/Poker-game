6a83396d47513d1532ff8d96b8cb18e1d6065b1e
Your Name <you@example.com>
2024-10-30 10:58:12 +0100

    creation deck of card

---
+++ poker_game.py
@@ -0,0 +1,7 @@
+deck_of_cards_notfull = ["As", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+colors = ["heart", "spade", "club", "diamond"]
+deck_of_cards = []
+for color in colors:
+    for card in deck_of_cards_notfull:
+        deck_of_cards.append(card + " of " + color)
+print(deck_of_cards)
